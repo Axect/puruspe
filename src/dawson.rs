5408f5decfbd2e8614bae2da3d44f11955b140e1
Tae Geun Kim <axect@outlook.kr>
2024-08-06 17:50:35 +0900

    Split dawson integral into dawson.rs

---
+++ src/dawson.rs
@@ -0,0 +1,56 @@
+use crate::utils::sign;
+use std::f64::consts::PI;
+
+// =============================================================================
+// Dawson's integral
+// =============================================================================
+/// Dawson's integral for real values.
+/// Accurate to about 2e-7 for small inputs.
+pub fn dawson(x: f64) -> f64 {
+    const NMAX: usize = 6;
+    let mut c = [0_f64; NMAX];
+    let h = 0.4;
+    let a1 = 2. / 3.;
+    let a2 = 0.4;
+    let a3 = 2. / 7.;
+    let n0: u64; // i is declared in the loops.
+    let mut d1: f64;
+    let mut d2: f64;
+    let mut e1: f64;
+    let e2: f64;
+    let mut sum: f64;
+    let x2: f64;
+    let xp: f64;
+    let xx: f64;
+    let ans: f64;
+
+    for i in 0..NMAX {
+        c[i] = f64::exp(-((2. * i as f64 + 1.) * h).powf(2.));
+    }
+
+    if x.abs() < 0.2 {
+        x2 = x * x;
+        ans = x * (1.0 - a1 * x2 * (1.0 - a2 * x2 * (1.0 - a3 * x2)));
+    } else {
+        xx = x.abs();
+        n0 = 2 * (0.5 * xx / h + 0.5) as u64; // Simply truncates the decimal places, as in NR.
+        xp = xx - n0 as f64 * h;
+        e1 = f64::exp(2.0 * xp * h);
+        e2 = e1 * e1;
+        d1 = n0 as f64 + 1.;
+        d2 = d1 - 2.0;
+        sum = 0.0;
+
+        for i in 0..NMAX {
+            d1 += 2.;
+            d2 -= 2.;
+            e1 *= e2;
+
+            sum += c[i] * (e1 / d1 + 1.0 / (d2 * e1));
+        }
+
+        ans = (1. / PI.sqrt()) * sign(f64::exp(-xp * xp), x) * sum;
+    }
+    return ans;
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::f64::consts::PI;
 
 // =============================================================================
 // Import from other crates
@@ -20,6 +19,9 @@ pub use beta::*;
 
 pub mod error;
 pub use error::*;
+
+pub mod dawson;
+pub use dawson::*;
 
 // =============================================================================
 // Constants
@@ -67,59 +69,6 @@ const W: [f64; 18] = [
     0.085346685739338721,
     0.085983275670394821,
 ];
-
-// =============================================================================
-// Dawson's integral
-// =============================================================================
-/// Dawson's integral for real values.
-/// Accurate to about 2e-7 for small inputs.
-pub fn dawson(x: f64) -> f64 {
-    const NMAX: usize = 6;
-    let mut c = [0_f64; NMAX];
-    let h = 0.4;
-    let a1 = 2. / 3.;
-    let a2 = 0.4;
-    let a3 = 2. / 7.;
-    let n0: u64; // i is declared in the loops.
-    let mut d1: f64;
-    let mut d2: f64;
-    let mut e1: f64;
-    let e2: f64;
-    let mut sum: f64;
-    let x2: f64;
-    let xp: f64;
-    let xx: f64;
-    let ans: f64;
-
-    for i in 0..NMAX {
-        c[i] = f64::exp(-((2. * i as f64 + 1.) * h).powf(2.));
-    }
-
-    if x.abs() < 0.2 {
-        x2 = x * x;
-        ans = x * (1.0 - a1 * x2 * (1.0 - a2 * x2 * (1.0 - a3 * x2)));
-    } else {
-        xx = x.abs();
-        n0 = 2 * (0.5 * xx / h + 0.5) as u64; // Simply truncates the decimal places, as in NR.
-        xp = xx - n0 as f64 * h;
-        e1 = f64::exp(2.0 * xp * h);
-        e2 = e1 * e1;
-        d1 = n0 as f64 + 1.;
-        d2 = d1 - 2.0;
-        sum = 0.0;
-
-        for i in 0..NMAX {
-            d1 += 2.;
-            d2 -= 2.;
-            e1 *= e2;
-
-            sum += c[i] * (e1 / d1 + 1.0 / (d2 * e1));
-        }
-
-        ans = (1. / PI.sqrt()) * sign(f64::exp(-xp * xp), x) * sum;
-    }
-    return ans;
-}
 
 // =============================================================================
 // Bessel functions
