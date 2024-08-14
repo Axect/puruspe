1fe8843cb87dfe7c151b10e7c14be6cc528762b5
Johanna Sörngård <jsorngard@gmail.com>
2024-08-14 15:36:16 +0200

    Add a test of the factorial function

---
+++ tests/factorial_test.rs
@@ -0,0 +1,14 @@
+use puruspe::utils::factorial;
+use approx::assert_ulps_eq;
+
+#[test]
+fn test_factorial() {
+    for i in 0..30 {
+        let mut fact = 1.0;
+        for j in 2..=i {
+            fact *= j as f64;
+        }
+        println!("{i}!");
+        assert_ulps_eq!(factorial(i), fact);
+    }
+}
