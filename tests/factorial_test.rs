ff2dc1c38a311747d8b3237d6a61f249f215310d
Johanna Sörngård <jsorngard@gmail.com>
2024-08-14 15:39:57 +0200

    simplify test code

--- tests/factorial_test.rs
+++ tests/factorial_test.rs
@@ -4,11 +4,9 @@ use approx::assert_ulps_eq;
 #[test]
 fn test_factorial() {
     for i in 0..30 {
-        let mut fact = 1.0;
-        for j in 2..=i {
-            fact *= j as f64;
-        }
-        
-        assert_ulps_eq!(factorial(i), fact);
+        assert_ulps_eq!(
+            factorial(i),
+            (2..=i).map(|j| j as f64).product()
+        );
     }
 }
