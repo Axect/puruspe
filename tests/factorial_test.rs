42300afd9411ac992442d6423625584f20f4944d
Johanna Sörngård <jsorngard@gmail.com>
2024-08-14 15:48:39 +0200

    Test to highest input that doesn't need custom epsilon

--- tests/factorial_test.rs
+++ tests/factorial_test.rs
@@ -3,7 +3,7 @@ use approx::assert_ulps_eq;
 
 #[test]
 fn test_factorial() {
-    for i in 0..30 {
+    for i in 0..90 {
         assert_ulps_eq!(
             factorial(i),
             (2..=i).map(|j| j as f64).product()
